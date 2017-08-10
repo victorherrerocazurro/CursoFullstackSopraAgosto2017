f15e7e33c27b563ae5d85ff6ab2cd149434ffd44
Victor <victorherrerocazurro@gmail.com>
2017-08-10 09:16:23 +0200

    Cloud y Documentacion

--- 10-Jpa-Data/src/main/java/com/sopra/jpa/data/H2Configuracion.java
+++ 10-Jpa-Data/src/main/java/com/sopra/jpa/data/H2Configuracion.java
@@ -1,6 +1,6 @@
 package com.sopra.jpa.data;
 
-import org.h2.server.web.WebServlet;
+//import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +8,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class H2Configuracion {
 
-	@Bean
+	/*@Bean
 	public ServletRegistrationBean h2ServletRegistration() {
 		return new ServletRegistrationBean(new WebServlet(), "/h2Console/*");
-	}
+	}*/
 	
 }
